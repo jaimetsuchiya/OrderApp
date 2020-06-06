2d989fefec12c797d7fd49b4213d2649f620e6c7
Jaime R. Tsuchiya <jaimetsuchiya@MacBook-Pro-de-Jaime.local>
2020-06-06 09:54:49 -0300

    Criacao do OrderApp + Setup DB

---
+++ Src/order.api/Controllers/SecurityController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using order.api.Domain.Commands;
+using order.api.Domain.Entities;
+using order.api.Infrastructure;
+
+namespace order.api.Controllers
+{
+    [Route("api/[controller]")]
+    public class SecurityController : Controller
+    {
+        [HttpGet]
+        public async Task<IEnumerable<Order>> GuestToken()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Order>> AdminToken()
+        {
+            return null;
+        }
+    }
+}
